a15a2a425fa5ac0814186709459592746226a7f4
Matías Cecchetto <matias.cec@gmail.com>
2018-03-10 19:23:29 +1000

    Test of different configuration

--- src/main/java/app/WebSocketConfig.java
+++ src/main/java/app/WebSocketConfig.java
@@ -12,9 +12,9 @@ import org.springframework.beans.factory.annotation.Value;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     // @Override
-    // public void configureMessageBroker(MessageBrokerRegistry config) {
-    //     config.enableSimpleBroker("/topic");
-    //     config.setApplicationDestinationPrefixes("/chat");
+    // public void configureMessageBroker(MessageBrokerRegistry registry) {
+    //     registry.enableSimpleBroker("/topic");
+    //     registry.setApplicationDestinationPrefixes("/chat");
     // }
 
     @Value("${spring.rabbitmq.host}")
@@ -28,21 +28,31 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-      registry.setApplicationDestinationPrefixes("/chat");
+
+      // registry.enableStompBrokerRelay("/topic")
+      //         .setRelayHost("furry-pipkin-43.bigwig.lshift.net")
+      //         .setVirtualHost("g0dTtITeQ9Ag")
+      //         .setRelayPort(10410)
+      //         .setClientLogin("AyGEPF8v")
+      //         .setClientPasscode("IGJDhxDhZtctiH9Pje1JgnH0AcYbNx-g");
 
       // Use this for enabling a Full featured broker like RabbitMQ
       registry.enableStompBrokerRelay("/topic")
               .setRelayHost(host)
+              // .setVirtualHost("/chat")
               .setRelayPort(port)
               .setClientLogin(login)
               .setClientPasscode(passcode);
+
+      registry.setApplicationDestinationPrefixes("/chat");
+
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
         // To enable CORS
-        // .setAllowedOrigins("*")
+        .setAllowedOrigins("*")
         .withSockJS();
         // The .setWebSocketEnabled(false) disables WebSockets in order to use XHR-Long Pooling
         // registry.addEndpoint("/ws").withSockJS().setWebSocketEnabled(false);
